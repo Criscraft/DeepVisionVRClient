2c0d4bb9d385c14456b3dd98aafc9b29f962371d
Christoph Linse <christophlinse@aol.com>
2022-05-31 18:14:04 +0200

    Added Button to toggle the visibility of network UI elements

--- DeepVisionVRClient/Assets/Scripts/NetworkSettingsButtons.cs
+++ DeepVisionVRClient/Assets/Scripts/NetworkSettingsButtons.cs
@@ -5,10 +5,22 @@ public class NetworkSettingsButtons : MonoBehaviour
     [SerializeField]
     private Transform network;
 
+    private bool networkUIVisible = true;
+
 
     public void OnNetworkScaleSliderChanged(float value)
     {
         network.localScale = new Vector3(value, value, value);
+    }
+
+
+    public void OnUIToggleButtonClick()
+    {
+        networkUIVisible = !networkUIVisible;
+        foreach (GameObject uiElement in GameObject.FindGameObjectsWithTag("ToggableNetworkUI"))
+        {
+            uiElement.SetActive(networkUIVisible);
+        }
     }
 
 }
